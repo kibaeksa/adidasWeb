422a800a6dcbaff8d142d52cdb43dea90fb69d42
kibaeksa <kibaek@santelglobal.com>
2017-05-18 14:51:14 +0900

    -

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var express = require('express');
 
 var app = express();
 var PORT = process.env.PORT || 20000;
-var rootPath = '/app/';
+var rootPath = '/';
 
-app.use('/',express.static(path.resolve(__dirname,'')));
+app.use('/',express.static(path.resolve(__dirname,'./app')));
 app.get('/',function(req,res){
-	 res.send('Adidas web project');
+	 res.send('Hello World!');
 });
-
 
 app.listen(PORT , function(){
 	console.log('Example app lsitening on port '+PORT+'!');
