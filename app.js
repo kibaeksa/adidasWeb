c4c2d24318cac7e17efddc41f3e7fc37f5d2a4cd
kibaeksa <kibaek@santelglobal.com>
2017-07-24 10:22:03 +0900

    - 코딩리스트 업데이트

--- app.js
+++ app.js
@@ -1,14 +1,158 @@
-var fs = require('fs');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
+var bodyParser = require('body-parser');
 var express = require('express');
+var htmlListJson = require('./app/data/codingListInfo.json');
+var objFromJson;
 
 var app = express();
-var PORT = process.env.PORT || 20000;
+var PORT = process.env.PORT || 2000;
 var rootPath = '/';
 
+
+function parseJsonData(target , source){
+    var key,
+        i = 0,
+        j = 0;
+
+    /* Indexing in particular object */
+    for(key in target){
+        if(source[key] && (source[key].path == target[key].path)){
+            /* Matching name property one by one which included  array in loop */
+            for(; i < target[key].htmlData.length; i++){
+                for(; j < source[key].htmlData.length; j++){
+                    if(target[key].htmlData[i].name == source[key].htmlData[j].name){
+
+                        if(!!source[key].htmlData[j].desc){
+                            target[key].htmlData[i].desc = source[key].htmlData[j].desc;
+
+                            j = 0;
+                            break;
+                        }
+                    }
+                }
+                j = 0;
+            }
+        }
+        i = 0;
+    }
+
+    return target;
+
+}
+
+function writeFile(data , cb){
+    fs.writeFile('./app/data/codingListInfo.json',data,function(err){
+        if(err) return console.log(err);
+        if(cb && typeof cb == 'function'){
+            cb();
+        }
+    });
+}
+
+
+function getFilesFromDir(fileInfo , targetObj){
+    /*
+        fileInfo : [
+            0 : Path
+            1 : Category name (It will be key of object)
+            2 : Root path
+        ]
+    */
+    var ctgrName;
+    var rootPath;
+    var dirCount = 0;
+    var htmlListData = targetObj ? targetObj : {};
+
+    if(fileInfo[1] == undefined){
+        ctgrName = 'root';
+        rootPath = '/';
+    }else{
+        ctgrName = fileInfo[1];
+        rootPath = fileInfo[2]
+    }
+
+    var fileList = fs.readdirSync(path.join(__dirname,fileInfo[0]));
+
+    fileList.forEach(function(file){
+
+        var stats = fs.statSync(path.join(__dirname,fileInfo[0]+file));
+
+        if(stats.isFile()  && !file.match(/\.html/) ){
+            return;
+        }
+
+        if(stats.isFile()){
+            var isExsist = false;
+
+            if(!htmlListData[ctgrName]){
+                htmlListData[ctgrName] = {
+                    path : rootPath
+                };
+            }
+
+            /* Checking whether it has already that file infomation */
+            if(htmlListData[ctgrName].htmlData){
+                for(var i = 0; i< htmlListData[ctgrName].htmlData.length; i++){
+                    if(htmlListData[ctgrName].htmlData[i].name == file){
+                        // console.log('Already exsist!');
+                        isExsist = true;
+                        break;
+                    }
+                }
+            }
+
+
+            if(!isExsist){
+                if(htmlListData[ctgrName].htmlData == undefined){
+                    htmlListData[ctgrName].htmlData = [];
+                }
+
+                htmlListData[ctgrName].htmlData.push({
+                    name : file
+                });
+
+                dirCount += 1;
+            }
+        }else{
+            getFilesFromDir([fileInfo[0]+file+'/' ,rootPath.replace(/^\//,'root___').replace(/\//,'___') + file, rootPath+file+'/'] , htmlListData);
+        }
+    });
+
+    return htmlListData;
+
+}
+/* getFilesFromDir Function END */
+
+
+function init(){
+    var newData = getFilesFromDir(['./app/html/adidas/']);
+    htmlList = parseJsonData(newData , htmlListJson);
+    writeFile(JSON.stringify(htmlList , null , 4));
+}
+
+init();
+
+
+
 app.use('/',express.static(path.resolve(__dirname,'./app')));
-app.get('/',function(req,res){
-	 res.send('Hello World!');
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+app.get('/html/adidas',function(req , res){
+    init();
+    res.sendFile(__dirname+'/public/html/index.html');
+});
+
+app.post('/modifyHtmlJson',function(req , res){
+    var data = {
+        objFromJson : objFromJson
+    };
+    htmlListJson = req.body.htmlList;
+    writeFile(JSON.stringify(req.body.htmlList , null , 4) , function(){
+        res.send(data);
+    });
 });
 
 app.listen(PORT , function(){
