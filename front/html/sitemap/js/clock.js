442e96f1e391c9cf689e6ccb6ded20ad85ec1f07
Seoyeonggyu <syg0713@gmail.com>
2019-06-21 15:23:23 +0900

    sitemap

---
+++ front/html/sitemap/js/clock.js
@@ -0,0 +1,26 @@
+
+            const clockContainer = document.querySelector('.js-clock'),
+            clockTitle = clockContainer.querySelector('h1');
+            function getTime(){
+                const date = new Date();
+                const minutes = date.getMinutes();
+                const hours = date.getHours();
+                const seconds = date.getSeconds();
+                
+                // 24시간제
+                clockTitle.innerText = 
+                `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}`:minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+                
+                // 12시간제
+                // clockTitle.innerText = 
+                // `${hours > 12 ? `${hours-12}` : hours}:${minutes < 10 ? `0${minutes}`:minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+
+
+            }
+
+            function init(){
+                getTime();
+                setInterval(getTime,1000);
+            };
+            init();
