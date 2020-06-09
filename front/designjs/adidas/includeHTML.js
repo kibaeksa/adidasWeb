1465a5d165ea132a93cfe95b75aa9aee6ae0cd56
syg0713 <syg0713@github.com>
2020-06-09 16:03:39 +0900

    adi_FRT_HTML 헤더/푸터 인클루드 작업

---
+++ front/designjs/adidas/includeHTML.js
@@ -0,0 +1,44 @@
+// [참고] https://www.w3schools.com/howto/howto_html_include.asp
+(function includeHTML(callback) {
+    var z, i, elmnt, file, xhrm;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName('*');
+    // header = document.getElementById("header_wrap");
+    // console.log(header)
+
+
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute('include-html');
+      //console.log(file);
+      if (file) {
+        /*make an HTTP request using the attribute value as the file name:*/
+        xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+              elmnt.innerHTML = this.responseText;
+              var scripts = elmnt.getElementsByTagName('script');
+              console.log(elmnt)
+              console.log(scripts)
+              for (var i = 0; i < scripts.length; i++) {
+                eval(scripts[i].text); 
+              }
+            }
+            if (this.status == 404) {elmnt.innerHTML = 'Page not found.';}
+            /*remove the attribute, and call this function once more:*/
+            elmnt.removeAttribute('include-html');
+            includeHTML(callback);
+          }
+        }      
+        xhr.open('GET', file, true);
+        xhr.send();
+        /*exit the function:*/
+       return;
+      }
+    }
+    setTimeout(function() {
+      callback();
+    }, 0)
+  })();
